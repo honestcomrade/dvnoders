1c8dde091e35290fe5780b0a5ed38227cdfe2532
Joe Saraceno <honestcomrade@gmail.com>
2017-11-01 11:35:41 -0700

    adds separate html file to serve using path module

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>')
+  res.sendFile(path.join(__dirname + '/dvnoders.html'));  
 })
 
 app.listen(8080, function () {
