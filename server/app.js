1db2b651c18bf1865f18f36966d5276e6c9785f2
Joe Saraceno <honestcomrade@gmail.com>
2017-11-01 15:12:34 -0700

    adds a page counter to show dynamic serving

--- server/app.js
+++ server/app.js
@@ -1,11 +1,22 @@
-var express = require('express')
-var path = require('path')
-var app = express()
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/../public/dvnoders.html'));  
-})
+var count = 0;
 
 app.listen(8080, function () {
   console.log('Node Server listening on port 8080')
 })
+
+app.get('/', function (req, res) {
+  count++;
+  fs.readFile('../public/dvnoders.html', 'utf8', function (err, data) {
+  
+    if (err) {
+      return console.log(err);
+    }
+
+    res.send(data.replace('{{counter}}', count));
+  });
+});
