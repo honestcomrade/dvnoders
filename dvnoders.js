42bd6f9d44b2d842627328b5b93a2baf61d12b95
Joe Saraceno <honestcomrade@gmail.com>
2017-11-01 11:18:55 -0700

    change to port that google cloud console supports

--- dvnoders.js
+++ dvnoders.js
@@ -2,9 +2,9 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('<h1>Hello World!</h1>')
 })
 
-app.listen(3000, function () {
-  console.log('Node Server listening on port 3000')
+app.listen(8080, function () {
+  console.log('Node Server listening on port 8080')
 })
